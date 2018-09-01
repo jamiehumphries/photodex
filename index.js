c63645359c96f252778b3b26823a8de7dfcff28d
Jamie Humphries <j@miehumphri.es>
2018-09-01 21:55:37 +0100

    Caching and error handling.

--- index.js
+++ index.js
@@ -14,34 +14,98 @@ const hbs = exphbs.create({ defaultLayout: 'main', helpers: handlebarsHelpers })
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-const flickerOptions = {
+const flickrOptions = {
   api_key: process.env.FLICKR_API_KEY,
   progress: false
 }
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.render('home', { subtitle: "Gotta snap 'em all!" })
 })
 
-const photodexPattern = new RegExp('phot[oó]dex', 'i')
-app.get('/:trainer', (req, res) => {
+app.get('/:trainer', async (req, res) => {
   const { trainer } = req.params
-  Flickr.tokenOnly(flickerOptions, (_, flickr) => {
-    flickr.people.findByUsername({ username: trainer }, (_, result) => {
-      const userId = result.user.nsid
-      flickr.photosets.getList({ user_id: userId }, (_, result) => {
-        const photodex = result.photosets.photoset.find(set => photodexPattern.test(set.title._content))
-        flickr.photosets.getPhotos({ user_id: userId, photoset_id: photodex.id, extras: 'url_m,url_l' }, (_, result) => {
-          const photoMap = getPhotoMap(result.photoset.photo)
-          const generations = GENERATIONS.map(gen => withDexEntries(gen, photoMap))
-          const snapCount = Object.keys(photoMap).length
-          const subtitle = `Snapped: ${snapCount}`
-          res.render('dex', { subtitle, generations, trainer, photoMap: JSON.stringify(photoMap) })
-        })
-      })
+  try {
+    const flickr = await getFlickr()
+    const userId = await findUser(flickr, trainer)
+    const photosetId = await findPhotodex(flickr, userId)
+    const photoset = await getPhotoset(flickr, userId, photosetId)
+    const photoMap = getPhotoMap(photoset.photo)
+    const generations = GENERATIONS.map(gen => withDexEntries(gen, photoMap))
+    const snapCount = Object.keys(photoMap).length
+    const subtitle = `Snapped: ${snapCount}`
+    res.render('dex', { trainer, subtitle, generations, photoMap: JSON.stringify(photoMap) })
+  } catch (error) {
+    const subtitle = '404: Not found!'
+    notFound(res, { trainer, subtitle, error: error.message })
+  }
+})
+
+function getFlickr () {
+  return new Promise((resolve, reject) => {
+    Flickr.tokenOnly(flickrOptions, (error, flickr) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(flickr)
+      }
     })
   })
-})
+}
+
+const findUserCache = {}
+function findUser (flickr, username) {
+  const cached = findUserCache[username]
+  if (cached) {
+    return Promise.resolve(cached)
+  }
+  return new Promise((resolve, reject) => {
+    flickr.people.findByUsername({ username }, (error, result) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(findUserCache[username] = result.user.nsid)
+      }
+    })
+  })
+}
+
+const PHOTODEX_REGEX = new RegExp('phot[oó]dex', 'i')
+const findPhotodexCache = {}
+function findPhotodex (flickr, userId) {
+  const cached = findPhotodexCache[userId]
+  if (cached) {
+    return Promise.resolve(cached)
+  }
+  return new Promise((resolve, reject) => {
+    flickr.photosets.getList({ user_id: userId }, (error, result) => {
+      if (error) {
+        reject(error)
+      } else {
+        const photosets = result.photosets.photoset
+        const photodex = photosets.find(set => PHOTODEX_REGEX.test(set.title._content))
+        if (photodex) {
+          resolve(findPhotodexCache[userId] = photodex.id)
+        } else {
+          reject(new Error("No public album found with 'Photódex' in the title"))
+        }
+      }
+    })
+  })
+}
+
+function getPhotoset (flickr, userId, photosetId) {
+  return new Promise((resolve, reject) => {
+    const params = { user_id: userId, photoset_id: photosetId, extras: 'url_m,url_l' }
+    flickr.photosets.getPhotos(params, (error, result) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result.photoset)
+      }
+    })
+  })
+}
 
 function getPhotoMap (photos) {
   const map = {}
@@ -74,6 +138,23 @@ function padNumber (n) {
   }
   return number
 }
+
+app.use((req, res, next) => {
+  notFound(res)
+})
+
+function notFound (res, context) {
+  const subtitle = '404: Not found!'
+  const error = 'No page exists at the requested route'
+  res.status(404).render('error', Object.assign({ subtitle, error }, context))
+}
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  const subtitle = '500: Server error!'
+  const error = 'An unexpected error has occurred'
+  res.status(500).render('error', { subtitle, error })
+})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}.`))
