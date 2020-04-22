5822f1d88019a3683916b7dda937407505855b7e
Jamie Humphries <jamie.humphries@outlook.com>
2020-04-22 21:26:20 +0100

    Fix Unova / Kalos generation boundary.

--- config/generations.js
+++ config/generations.js
@@ -3,8 +3,8 @@ module.exports = Object.freeze([
   { region: 'Johto', start: 152, end: 251 },
   { region: 'Hoenn', start: 252, end: 386 },
   { region: 'Sinnoh', start: 387, end: 493 },
-  { region: 'Unova', start: 494, end: 648 },
-  { region: 'Kalos', start: 649, end: 721 },
+  { region: 'Unova', start: 494, end: 649 },
+  { region: 'Kalos', start: 650, end: 721 },
   { region: 'Alola', start: 722, end: 807 },
   { region: 'Unknown', start: 808, end: 809 }
 ])
