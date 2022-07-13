5a827cce32b6aeb44d6ab27a545565c49a24371e
Matt Kataryniak <mattzapp17@gmail.com>
2022-07-13 12:18:21 -0400

    Added SW/SH DLC mons and Hisui mons

--- config/generations.js
+++ config/generations.js
@@ -7,5 +7,6 @@ module.exports = Object.freeze([
   { region: 'Kalos', start: 650, end: 721 },
   { region: 'Alola', start: 722, end: 807 },
   { region: 'Unknown', start: 808, end: 809 },
-  { region: 'Galar', start: 810, end: 890 }
+  { region: 'Galar', start: 810, end: 898 },
+  { region: 'Hisui', start: 899, end: 905 }
 ])
